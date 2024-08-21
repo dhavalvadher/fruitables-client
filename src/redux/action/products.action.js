b1be882bb3e36c3d0cb61ae52979a8e7fd721174
dhavalvadher <dhavalvadher0001@gmail.com>
2024-08-21 11:17:02 +0530

    i

--- src/redux/action/products.action.js
+++ src/redux/action/products.action.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { GET_PRODUCTS, ADD_PRODUCTS, DELETE_PRODUCTS, EDIT_PRODUCTS, LOADING_PRODUCTS, ERROR_PRODUCTS } from '../ActionType';
+import { baseURL } from '../../Utils/baseURL';
 
 
 export const getProducts = () => async (dispatch) => {
     dispatch({ type: LOADING_PRODUCTS });
     try {
-        const response = await axios.get("http://localhost:9000/api/v1/products/list-products");
+        const response = await axios.get(baseURL + "products/list-products");
         dispatch({ type: GET_PRODUCTS, payload: response.data.data });
     } catch (error) {
         dispatch({ type: ERROR_PRODUCTS, payload: error.message });
@@ -17,7 +18,7 @@ export const addProducts = (product) => async (dispatch) => {
     console.log(product);
     dispatch({ type: LOADING_PRODUCTS });
     try {
-        const response = await axios.post("http://localhost:9000/api/v1/products/create-product", product, {
+        const response = await axios.post(baseURL + "products/create-product", product, {
             headers: { 'Content-Type': 'multipart/form-data'},
         });
 
@@ -34,7 +35,7 @@ export const addProducts = (product) => async (dispatch) => {
 export const editProducts = (product) => async (dispatch) => {
     dispatch({ type: LOADING_PRODUCTS });
     try {
-        const response = await axios.put(`http://localhost:9000/api/v1/products/update-product/${product._id}`, product,{
+        const response = await axios.put(baseURL + `products/update-product/${product._id}`, product,{
             headers: { 'Content-Type': 'multipart/form-data'},
         });
 
@@ -48,7 +49,7 @@ export const editProducts = (product) => async (dispatch) => {
 export const deleteProducts = (id) => async (dispatch) => {
     dispatch({ type: LOADING_PRODUCTS });
     try {
-        await axios.delete(`http://localhost:9000/api/v1/products/delete-product/${id}`);
+        await axios.delete(baseURL + `products/delete-product/${id}`);
         dispatch({ type: DELETE_PRODUCTS, payload: id });
     } catch (error) {
         dispatch({ type: ERROR_PRODUCTS, payload: error.message });
