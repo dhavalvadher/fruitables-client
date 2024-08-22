d19c745749537ba73aa1779c05f839554eb26422
dhavalvadher <dhavalvadher0001@gmail.com>
2024-08-22 10:01:50 +0530

    jh

--- src/redux/action/category.action.js
+++ src/redux/action/category.action.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { ADD_CATEGORY, DELETE_CATEGORY, EDIT_CATEGORY, ERROR_CATEGORY, GET_CATEGORY } from '../ActionType';
-import { baseURL } from '../../Utils/baseURL';
+// import { baseURL } from '../../Utils/baseURL';
+import axiosInstance from '../../Utils/axiosInstance';
 
 
 
@@ -11,7 +12,7 @@ export const setError = (error) => ({ type: ERROR_CATEGORY, payload: error });
 
 export const getCategories = () => async (dispatch) => {
     try {
-        const response = await axios.get(baseURL + "categories/list_categories");
+        const response = await axiosInstance.get("categories/list_categories");
         dispatch({ type: GET_CATEGORY, payload: response.data });
     } catch (error) {
         dispatch(setError(error.message));
@@ -21,7 +22,7 @@ export const getCategories = () => async (dispatch) => {
 export const addCategory = (data) => async (dispatch) => {
     
     try {
-        const response = await axios.post(baseURL + "categories/post_categories", data);
+        const response = await axiosInstance.post("categories/post_categories", data);
         dispatch({ type: ADD_CATEGORY, payload: response.data });
     } catch (error) {
         dispatch(setError(error.message));
@@ -31,7 +32,7 @@ export const addCategory = (data) => async (dispatch) => {
 export const deleteCategory = (id) => async (dispatch) => {
     
     try {
-        await axios.delete(baseURL + "categories/delete_categories/" + id);
+        await axiosInstance.delete("categories/delete_categories/" + id);
         dispatch({ type: DELETE_CATEGORY, payload: id });
     } catch (error) {
         dispatch(setError(error.message));
@@ -41,7 +42,7 @@ export const deleteCategory = (id) => async (dispatch) => {
 export const editCategory = (data) => async (dispatch) => {
     
     try {
-        const response = await axios.put(baseURL + "categories/update_categories/" + data._id, data);
+        const response = await axiosInstance.put("categories/update_categories/" + data._id, data);
         dispatch({ type: EDIT_CATEGORY, payload: response.data });
     } catch (error) {
         dispatch(setError(error.message));
